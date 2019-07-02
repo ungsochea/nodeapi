edc3566e7a7ac9fad450e28386e98a6985c8e1bb
ungsochea <sochea.ung@gmail.com>
2019-07-02 15:55:10 +0700

    get all post and post by user

--- controllers/post.js
+++ controllers/post.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 
 exports.getPosts = (req,res)=>{
     const posts = Post.find()
+    .populate("postedBy","_id name")
+    .select("_id title body")
     .then(posts=>{
         res.json({posts});
     })
@@ -24,7 +26,7 @@ exports.createPost = (req,res,next) =>{
         req.profile.hashed_password = undefined;
         req.profile.salt = undefined;
         post.postedBy = req.profile;
-        
+
         if(files.photo){
             post.photo.data = fs.readFileSync(files.photo.path)
             post.photo.contentType = files.photo.type
@@ -45,4 +47,18 @@ exports.createPost = (req,res,next) =>{
     //         post:result
     //     });
     // });
+}
+
+exports.postsByUser = (req,res)=>{
+    Post.find({postedBy:req.profile._id})
+    .populate("postedBy","_id name")
+    .sort("_created")
+    .exec((err,posts)=>{
+        if(err){
+            return res.status(400).json({
+                error:err
+            })
+        }
+        res.json(posts)
+    })
 }
--- routes/post.js
+++ routes/post.js
@@ -1,13 +1,14 @@
 const express = require('express') 
-const {getPosts,createPost} = require('../controllers/post')
+const {getPosts,createPost,postsByUser} = require('../controllers/post')
 const { requireSignin } = require('../controllers/auth')
 const { createPostValidator } = require('../validator/index')
 const { userById } = require('../controllers/user')
 
 const router = express.Router()
 
-router.get('/',getPosts)
-router.post('/post/new/:userId',requireSignin,createPost,createPostValidator)
+router.get('/',getPosts);
+router.post('/post/new/:userId',requireSignin,createPost,createPostValidator);
+router.get('/posts/by/:userId',requireSignin,postsByUser);
 
 // any route contaning :uerId our app will first execute userById()
 router.param("userId",userById);
