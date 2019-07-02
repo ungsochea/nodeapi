ca809e3c7a5c09bf7b730a31f857a0d34a07604a
ungsochea <sochea.ung@gmail.com>
2019-07-02 14:30:22 +0700

    update user

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const User = require("../models/user")
 
 exports.userById = (req,res,next,id) =>{
@@ -37,4 +38,20 @@ exports.getUser = (req,res) => {
     req.profile.hashed_password = undefined;
     req.profile.salt = undefined;
     return res.json(req.profile);
+};
+
+exports.updateUser = (req,res) => {
+    let user = req.profile
+    user = _.extend(user,req.body) // extend - mutate the source object
+    user.updated = Date.now()
+    user.save((err)=>{
+        if(err){
+            return res.status(400).json({
+                error:"You are not authorized to perform this action"
+            })
+        }
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        res.json({user})
+    })
 }
